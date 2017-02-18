2c6d54880007038f955e48536419491cc508c550
GCR-B17\ladmin <standchat0820@gmail.com>
2017-02-18 19:22:00 +0900

    最新版マージ

---
+++ Assets/Tachibana/PlayrerCharacterOverLap.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayrerCharacterOverLap : MonoBehaviour
+{
+
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            TransitionManager.I.FadeOut(1.0f);
+            GameData.WinnerIsIron = true;
+            UnityEngine.SceneManagement.SceneManager.LoadSceneAsync("result");
+        }
+        
+        if(col.gameObject.tag == "Goal")
+        {
+            TransitionManager.I.FadeOut(1.0f);
+            GameData.WinnerIsIron = false;
+            UnityEngine.SceneManagement.SceneManager.LoadSceneAsync("result");
+        }
+    }
+}
---
+++ Assets/Tachibana/Scripts/ResultManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResultManager : MonoBehaviour {
+
+    [SerializeField]
+    Text text;
+
+	// Use this for initialization
+	void Start () {
+		if(GameData.WinnerIsIron)
+        {
+            text.text = "アイロンの勝ち！";
+        }
+        else
+        {
+            text.text = "ユニティちゃんの勝ち！";
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+		if(Input.GetButtonDown("Fire1"))
+        {
+            TransitionManager.I.FadeOut(1.0f);
+            UnityEngine.SceneManagement.SceneManager.LoadSceneAsync("Title");
+        }
+	}
+}
