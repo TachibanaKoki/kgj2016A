4c50bf7f491f49da358c31bf681ea1e4f100f9d7
yuki-takamura <g1527047@st.t-kougei.ac.jp>
2017-02-19 11:43:49 +0900

    AI追加(Z軸往復)

---
+++ Assets/Takamura/CreateLimitedDummy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateLimitedDummy : MonoBehaviour
+{
+
+    public GameObject chara;
+    public float interval = 3;
+    private float time;
+    //public int numberOfDummy;
+    public int numberOfDummyMax = 5;
+    private int count = 0;
+
+    public Vector3 createPosition;
+
+    // Use this for initialization
+    void Update()
+    {
+        time += Time.deltaTime;
+
+        if (time >= interval && count < numberOfDummyMax)
+        {
+            time = 0;
+            CreateCharacter();
+        }
+    }
+
+    void CreateCharacter()
+    {
+        float randomWidth;
+        randomWidth = Random.Range(-5, 5);
+        createPosition = new Vector3(randomWidth,0,-5);
+
+        Instantiate(chara, createPosition, Quaternion.identity);
+        count++;
+    }
+}
---
+++ Assets/Takamura/DummyMove3.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DummyMove3 : MonoBehaviour {   
+    private float angle;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        angle += 0.1f;
+        if (angle >= 179)
+            angle = 0;
+
+        transform.Translate(new Vector3(0, 0, Mathf.Sin(angle) * 0.3f));
+    }
+}
